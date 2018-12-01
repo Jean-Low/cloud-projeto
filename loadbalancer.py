be7926f6597f8c678da10b59cfca6d4dd8410db3
jean walper <jeanlow@al.insper.edu.br>
2018-12-01 11:35:54 -0200

    finished loadbalancer local code

--- loadbalancer.py
+++ loadbalancer.py
@@ -4,6 +4,8 @@ import boto3
 import time
 import sys
 import random
+import requests as r
+from threading import Timer
 from botocore.exceptions import ClientError
 
 #--- paths
@@ -11,8 +13,8 @@ from botocore.exceptions import ClientError
 app = Flask(__name__)
 api = Api(app)
 
-access_key = sys.argv[1]
-secret_key = sys.argv[2]
+access_key = sys.argv[2]
+secret_key = sys.argv[3]
 
 
 ec2Resource = boto3.resource('ec2', aws_access_key_id = access_key, aws_secret_access_key = secret_key)
@@ -23,8 +25,55 @@ waiter = ec2.get_waiter('instance_terminated')
 
 #--- Vars
 
-active_instances = {}
+desired_amount = int(sys.argv[1]);
 
+#--- Init
+
+userdataMockUp ='''#!/bin/bash
+cd home/ubuntu
+mkdir running
+git clone https://github.com/Lightclawjl/cloud-projeto
+cd cloud-projeto
+bash install.sh
+python3 rest.py
+
+'''
+
+
+#--- Managers
+
+def create_instances(amount):
+    print("--- Creating {} Instance(s)".format(amount))
+    warning = None
+    userdata = userdataMockUp
+    response = ec2Resource.create_instances(
+
+                        ImageId = 'ami-0ac019f4fcb7cb7e6',
+                        MinCount=amount,
+                        MaxCount=amount,
+                        KeyName="despair",
+                        SecurityGroups=[
+                            'APS_Jean',
+                        ],
+                        InstanceType="t2.micro",
+                        UserData=userdataMockUp,
+                        TagSpecifications=[
+                                    {
+                                        'ResourceType' : 'instance',
+                                        'Tags': [
+                                            {
+                                                'Key': 'owner',
+                                                'Value': 'jean'
+                                            },
+                                            {
+                                                'Key': 'loader',
+                                                'Value': 'False'
+                                            },
+                                        ]
+                                    },
+                                ]
+                    )
+    return(True, response, warning)
 
 ###Endpoints###
 @app.route('/', defaults={"path":''})
@@ -33,29 +82,146 @@ def catch_all(path):
     if(path == "healthcheck"):
         return ('ok',200)
     
-    choice = random.choice(list(active_instances.values()))
+    choice = random.choice(list(active_instances.keys()))
     
     if(choice == None):
         return ('error',400)
     newpath = "http://" + choice + ":5000/" + path
     return redirect(newpath, code=302)
 
-#api.add_resource(Tarefa,'/Tarefa/<int:id>', endpoint = 'Tarefa')
-#api.add_resource(Healthcheck,'/healthcheck', endpoint = 'healthcheck')
+
+def update():
+    global setup_instances
+    print("Status checking")
+    iplist = active_instances.keys()
+    ips_to_remove = []
+    for ip in iplist:
+        print("Checking {}".format(ip))
+        test = "http://{}:5000/healthcheck".format(ip)
+        #print(test)
+        passed = False
+        try: 
+            r.get(test,timeout = 3.0)
+            passed = True
+        except:
+            passed = False
+        if(passed):
+            print("Ok")
+        else:
+            print("Malfunction")
+            print("--Removing {}".format(ip))
+            ec2.terminate_instances(InstanceIds=[active_instances[ip]])
+            ips_to_remove.append(ip)
+            
+    for ip in ips_to_remove:
+        active_instances.pop(ip,None)
+    
+    if len(active_instances.keys()) != desired_amount :
+        print("There are {} machines".format(len(active_instances)))
+        print("Correcting machines to desired amount ({})".format(desired_amount))
+        dif = desired_amount - len(active_instances.keys())
+        if(dif < 0):
+            print("The are more machines than desired, reducing")
+            iplist = active_instances.keys()
+            for ip in iplist:
+                print("--Removing {}".format(ip))
+                instanceIds = []
+                instanceIds.append(active_instances[ip])
+                print(instanceIds)
+                try:
+                    ec2.terminate_instances(InstanceIds= instanceIds)
+                except:
+                    print("Could not terminate instance {}".format(ip))
+                    print("Manual check required")
+                    print("Removed from loadbalacer anyways")
+                active_instances.pop(ip,None)
+                print("Removed")
+                dif += 1
+                if dif == 0:
+                    break
+                    
+        elif (dif > 0):
+            if(len(setup_instances) > 0):
+                amount = len(setup_instances)
+                print("There are {} machines are in set up stage".format(amount))
+                for i in range(amount):
+                    setup_instances[i][0].load()
+                    
+                    ip = setup_instances[i][0].public_ip_address
+                    up = setup_instances[i][1]
+                    if(up):
+                        test = "http://{}:5000/healthcheck".format(ip)
+                        passed = False
+                        try: 
+                            r.get(test,timeout = 3.0)
+                            passed = True
+                        except:
+                            pass
+                        
+                        if(passed):
+                            print("A new machine is running. ({})".format(setup_instances[i][0].id))
+                            active_instances[ip] = setup_instances[i][0].id
+                            setup_instances.pop(i)
+                            print(setup_instances)
+                            print(active_instances)
+                        else:
+                            print("Machine {} is still waiting for server...".format(setup_instances[i][0].id))
+                            
+                    elif (ip != None):
+                        setup_instances[i][1] = True
+                        print("Machine {} is up, waiting for server".format(setup_instances[i][0].id))
+                        
+
+                
+            else:
+                print("There are less machines than desired, increasing")
+                response = create_instances(dif)[1]
+                setup_instances = []
+                for i in response:
+                    setup_instances.append([i,False])
+                print("Launching {} new instances".format(len(setup_instances)))
+                
+            #while (dif != 0):
+                #print("--Adding")
+                #response = create_instance()
+                #print(response[1])
+                #print(ip + " -- " + response[1][0].id)
+                #active_instances[ip] = response[1][0].public_ip_address
+                #print(active_instances.keys())
+                #dif -= 1
+        print("Now there are {} machines".format(len(active_instances.keys())))
+    print("Done!")
+    Timer(10.0, update).start()
 
 #Run routine
 
-print("I work")
+print("Running load balancer")
 
-for i in ec2Resource.instances.filter(Filters=[{  
+active_instances = {}
+setup_instances = [] #format (instace, times until timeout)
+
+
+for i in ec2Resource.instances.filter(Filters=[{ 
         'Name' : 'instance-state-name', 
         'Values' : ['running']
+        },
+        { 
+        'Name' : 'tag:owner', 
+        'Values' : ['jean']
+        },
+        {
+        'Name' : 'tag:loader', 
+        'Values' : ['False']
         }]):
-            active_instances[i.id] = i.public_ip_address
+            print("makina")
+            print(i.id)
+            active_instances[i.public_ip_address] = i.id
 
-print(active_instances)
+print(active_instances.keys())
         
 
+#start check
+Timer(2.0,update).start()
 
 if __name__ == '__main__':
     app.run(debug = False, host="0.0.0.0", port=5000)
